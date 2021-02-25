f872803885b11710a1cf45a4b7f164bd518aea92
Swati Makhija <swatimakhija@Swatis-MacBook-Air.local>
2021-02-24 16:00:43 -0800

    added change body button function

--- src/App.js
+++ src/App.js
@@ -10,24 +10,28 @@ class App extends Component {
       comments: ['First!', 'Great post', 'Hire this author now!']
   }
   
+  //Method that will update 'body' state
+  //Methods will usually go outside of render()
+  changeBody = () => {
+    const userInput = prompt("give a new body")
+    console.log(userInput)
+    //update body state
+    this.setState({ body: userInput })
+  }
+
   render() {
-    const post = {
-      title: "Dinosaurs are awesome",
-      author: "Stealthy Stegosaurus",
-      body: "Checkout out this body property",
-      comments: ['First!', 'Great post', 'Hire this author now!']
-    }
     // map returns an array 
-    const comments = post.comments.map((comment, index)=> (<Comment text={comment} key={index}/>))
+    const comments = this.state.comments.map((comment, index)=> (<Comment text={comment} key={index}/>))
   
     return (
       <div className="App">
         <h1>{this.state.title}</h1>
-        <p>by {post.author}</p>
-        <p>{post.body}</p>
+        <p>by {this.state.author}</p>
+        <p>{this.state.body}</p>
         <h3>Comments:</h3>
-        <p>{post.comments[0]}</p>
+        {/* <p>{post.comments[0]}</p> WHY IS THIS COMMENTED OUT*/} 
         { comments }
+        <button onClick={this.changeBody}>Change body</button>
       </div>
     );
   }
